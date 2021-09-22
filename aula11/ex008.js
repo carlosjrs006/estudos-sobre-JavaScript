0c97a69a4112b801d883c12da2a3d2bfaff65078
carlosjunio <carlosjrs06.cj@gmail.com>
2021-09-21 21:30:04 -0300

    Estudos sobre JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // condicao simples
+    console.log('Voce ultrapassou a velocidade permitida.MULTADO!!!')
+}
+console.log('Dirija sempre usando cindo de seguranca!')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA' 
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('voce é brasileiro!')
+}
+else{
+    console.log(' voce é Estrangeiro!')
+}
